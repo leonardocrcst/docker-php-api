c02eb9fe96e96f606290b3cfae58be9080fe7e86
Léo Cruz <leikovsk@hotmail.com>
2024-08-29 11:44:44 -0300

    Access control e sessão

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+session_start();
+
 use DI\Container;
 use Slim\Factory\AppFactory;
 
